d4f9310e5c77a939486e0d8301eecdf93b72de33
Fathya Azra Nazmira 039 <108808542+FathyaAzra@users.noreply.github.com>
2024-12-09 19:36:08 +0700

    Gradle and Manifest setting

---
+++ app/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id ("com.google.devtools.ksp")
+    alias(libs.plugins.androidx.navigation.safeargs.kotlin)
+}
+
+android {
+    namespace = "net.wadidaw.newsapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "net.wadidaw.newsapp"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Architectural Components
+    implementation(libs.lifecycle.viewmodel.ktx)
+
+    // Room
+    implementation(libs.room.runtime)
+    ksp(libs.room.compiler)
+
+    // Kotlin Extensions and Coroutines support for Room
+    implementation(libs.room.ktx)
+
+    // Coroutines
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.okhttp.logging.interceptor)
+
+    // Navigation Components
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
+
+    // Glide
+    implementation(libs.glide)
+    ksp(libs.glide.compiler)
+}
