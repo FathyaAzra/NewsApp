b15a9e12ad87e7c0bd012144335b80158e92fbb2
Fathya Azra Nazmira 039 <108808542+FathyaAzra@users.noreply.github.com>
2024-12-14 17:30:40 +0700

    Fragment

--- app/src/main/java/net/wadidaw/newsapp/util/Constants.kt
+++ app/src/main/java/net/wadidaw/newsapp/util/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object{
         const val API_KEY = "d7fa99d464a04f688b17b932e405ea89"
         const val BASE_URL = "https://newsapi.org/"
-        const val SEARCH_NEWS_TIME_DELAY = "500L"
-        const val QUERY_PAGE_SIZE = "20"
+        const val SEARCH_NEWS_TIME_DELAY: Long = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
